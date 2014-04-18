8e30faddf92edd59afb4a3f0e9c5df7539d37f2e
jaggr2 <jaggr2@bfh.ch>
2014-04-18 01:52:32 +0000

    improved button detection

--- routes/relay.js
+++ routes/relay.js
@@ -3,12 +3,26 @@
  * GET users listing.
  */
 
+var ledstatus = 0;
+var lastchangeddate = (new Date()).getTime();
+
 var Gpio = require('onoff').Gpio,
     led = new Gpio(17, 'out'),
-    button = new Gpio(18, 'in', 'both');
+    button = new Gpio(18, 'in');
 
 button.watch(function(err, value) {
-    led.writeSync(value);
+    console.log(new Date() + 'Button value: ' + value);
+
+    if(value == 1 && (new Date()).getTime() - 250 > lastchangeddate) {
+    if(ledstatus == 0 ) {
+	ledstatus = 1;
+    } else {
+	ledstatus = 0;
+    }
+    lastchangeddate = (new Date()).getTime();
+
+    led.writeSync(ledstatus);
+    }
 });
 
 
