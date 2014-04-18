148cac822b87923ef198809652e39b7bfd04b36a
roger <roger.jaggi@blastbeatsyndicate.com>
2014-04-18 13:21:35 +0200

    improved relay

--- routes/relay.js
+++ routes/relay.js
@@ -3,27 +3,37 @@
  * GET users listing.
  */
 
-var ledstatus = 0;
-var lastchangeddate = (new Date()).getTime();
+var ledStatus = 0;
+var lastChangeDate = (new Date()).getTime();
 
 var Gpio = require('onoff').Gpio,
     led = new Gpio(17, 'out'),
     button = new Gpio(18, 'in');
 
 button.watch(function(err, value) {
+    if (err) exit();
+
     console.log(new Date() + 'Button value: ' + value);
 
-    if(value == 1 && (new Date()).getTime() - 250 > lastchangeddate) {
-    if(ledstatus == 0 ) {
-	ledstatus = 1;
-    } else {
-	ledstatus = 0;
-    }
-    lastchangeddate = (new Date()).getTime();
+    if(value == 1 && (new Date()).getTime() - 250 > lastChangeDate) {
+        if(ledStatus == 0 ) {
+            ledStatus = 1;
+        } else {
+            ledStatus = 0;
+        }
+        lastChangeDate = (new Date()).getTime();
 
-    led.writeSync(ledstatus);
+        led.writeSync(ledStatus);
     }
 });
+
+function exit() {
+    led.unexport();
+    button.unexport();
+    process.exit();
+}
+
+process.on('SIGINT', exit);
 
 
 exports.index = function(req, res){
